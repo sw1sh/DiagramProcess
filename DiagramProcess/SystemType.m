926f9f049049701d83b5388948cc28b4843a2e8e
Nikolay Murzin <murzin.nikolay@gmail.com>
2020-11-03 21:10:08 +0300

    Add type dimensionality

--- DiagramProcess/SystemType.m
+++ DiagramProcess/SystemType.m
@@ -6,25 +6,30 @@ PackageScope["dualTypeQ"]
 PackageScope["dualTypesQ"]
 PackageScope["dualType"]
 PackageScope["typeArity"]
+PackageScope["typeDimensions"]
+PackageScope["typeBasis"]
 
 
 SystemType::usage = "A representation of types for process inputs and outputs"
 
-Options[SystemType] = {"Dual" -> False};
+Options[SystemType] = {"Dual" -> False, "Dimensions" -> {2}, "Field" -> Complexes};
 
-SystemType[type_] := SystemType[type, Sequence @@ Options[SystemType]]
+SystemType[type : Except[_SystemType]] := SystemType[type, Sequence @@ Options[SystemType]]
 
-SystemType[CircleTimes[ts__]] := CircleTimes @@ Map[SystemType, {ts}]
+SystemType[type : Except[_SystemType], opts : OptionsPattern[]] /; ! ContainsAll[Keys[{opts}], Keys[Options[SystemType]]] :=
+    SystemType[type, opts, Sequence @@ FilterRules[Options[SystemType], Except[{opts}]]]
 
-SystemType /: (t : CircleTimes[__SystemType]) := SystemType[Defer[t]]
+SystemType[CircleTimes[ts__], opts : OptionsPattern[]] := CircleTimes @@ Map[SystemType[#, opts] &, {ts}]
 
-SystemType[SuperStar[type_], opts : OptionsPattern[]] := SuperStar[SystemType[type]]
+SystemType /: (t : CircleTimes[ts__SystemType]) := SystemType[Defer[t],
+    "Dimensions" -> Catenate[typeDimensions /@ {ts}], "Field" -> Apply[CircleTimes, typeField /@ {ts}]]
+
+SystemType[SuperStar[type_], opts : OptionsPattern[]] := SuperStar[SystemType[type, opts]]
 
 SystemType /: SuperStar[t_SystemType] := dualType @ t
 
-SystemType[t_SystemType] := t
 
-SystemType[SystemType[t_, ___], args__] := SystemType[t, args]
+SystemType[SystemType[t_, args1___], args2___] := SystemType[t, Sequence @@ Normal @ Merge[Join[{args2}, {args1}], First]]
 
 
 typeLabel[type : SystemType[t_, ___]] := If[dualTypeQ[type], SuperStar[getLabel[t]], getLabel[t]]
@@ -33,32 +38,46 @@ typeLabel[SystemType[Defer[CircleTimes[t_SystemType, t_SystemType]], ___]] := Ov
 
 
 dualTypeQ[SystemType[_, opts : OptionsPattern[]]] :=
-    TrueQ[OptionValue[SystemType, opts, "Dual"]]
+    TrueQ[OptionValue[SystemType, {opts}, "Dual"]]
 
 
 dualType[SystemType[t : Except[_Defer], opts : OptionsPattern[SystemType]]] :=
     SystemType[t,
-      "Dual" -> Not[OptionValue[SystemType, opts, "Dual"]],
-      Sequence @@ FilterRules[opts, Except["Dual"]]]
+      "Dual" -> Not[OptionValue[SystemType, {opts}, "Dual"]],
+      Sequence @@ FilterRules[{opts}, Except["Dual"]]]
 
-dualType[SystemType[Defer[CircleTimes[ts__]] | Labeled[Defer[CircleTimes[ts__]], _], ___]] := Apply[CircleTimes, dualType /@ {ts}]
+dualType[SystemType[Defer[CircleTimes[ts__]], ___]] := Apply[CircleTimes, dualType /@ {ts}]
 
 
 typeArity[_SystemType] := 1
 
-typeArity[SystemType[Defer[CircleTimes[ts__]] | Labeled[Defer[CircleTimes[ts__]], _], ___]] := Total[typeArity /@ {ts}]
+typeArity[SystemType[Defer[CircleTimes[ts__]], ___]] := Total[typeArity /@ {ts}]
 
 
-dualTypesQ[SystemType[Defer[CircleTimes[ts__]] | Labeled[Defer[CircleTimes[ts__]], _], ___]] := dualTypeQ /@ {ts}
+dualTypesQ[SystemType[Defer[CircleTimes[ts__]], ___]] := dualTypeQ /@ {ts}
 
 dualTypesQ[t_SystemType] := {dualTypeQ[t]}
 
 
+typeDimensions[SystemType[_, opts : OptionsPattern[]]] := OptionValue[SystemType, {opts}, "Dimensions"]
+
+
+typeField[SystemType[_, opts : OptionsPattern[]]] := OptionValue[SystemType, {opts}, "Field"]
+
+
+typeBasis[t_SystemType] := With[{dims = typeDimensions[t]},
+    SparseArray[{IntegerDigits[#, MixedRadix[dims], Length[dims]] + 1 -> 1}, dims] & /@ Range[0, Times @@ dims - 1]
+]
+
+
 (* Boxes *)
-MakeBoxes[type : SystemType[_, OptionsPattern[]], form_] :=
-    ToBoxes[
-        Tooltip[
-            typeLabel[type],
-            Row[{typeLabel[type], ":", If[dualTypeQ[type], SuperStar["Type"], "Type"]}]
-        ],
-    form]
+MakeBoxes[type : SystemType[_, OptionsPattern[]], form_] := With[{
+    boxes = ToBoxes[typeLabel[type], form],
+    tooltip = ToBoxes[Superscript[If[dualTypeQ[type], SuperStar[typeField[type]], typeField[type]], CircleTimes @@ typeDimensions[type]], form]
+},
+    InterpretationBox[
+        boxes,
+        type,
+        Tooltip -> tooltip
+    ]
+]
